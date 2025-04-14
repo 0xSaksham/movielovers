3d08c12141d0afb3cc2d87a09302842897fa283e
Saksham Gupta <sakshamgupta.ai@gmail.com>
2025-04-14 16:38:34 +0530

    Basic Setup Completed

---
+++ app/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="flex-1 items-center justify-center">
+      <Text className="text-4xl font-bold text-blue-500">Jai Shree Krishna</Text>
+    </View>
+  );
+}
